396de659d80e5d28473f5d0998b4e083927cc4c6
HenryHu-H <3190105630@zju.edu.cn>
2023-05-22 11:47:47 +0800

    debug

--- src/unifed/frameworks/federatedscope/protocol.py
+++ src/unifed/frameworks/federatedscope/protocol.py
@@ -286,10 +286,10 @@ def run_external_process_and_collect_result(cl: CL.CoLink, participant_id,  role
         returncode = process.returncode
         with open(temp_output_filename, "rb") as f:
             output = f.read()
-        cl.create_entry(f"{UNIFED_TASK_DIR}:{cl.get_task_id()}:output", output)
+        cl.create_entry(f"{UNIFED_TASK_DIR}:{cl.get_task_id()}:output", stdout.decode())
         with open(temp_log_filename, "rb") as f:
             log = f.read()
-        cl.create_entry(f"{UNIFED_TASK_DIR}:{cl.get_task_id()}:log", log)
+        cl.create_entry(f"{UNIFED_TASK_DIR}:{cl.get_task_id()}:log", stderr.decode())
         return json.dumps({
             "server_ip": server_ip,
             "stdout": stdout.decode(),
